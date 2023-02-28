a2ffad47223bdb04121d0e6b515544370edd5468
rakeshyadav-de <68526480+rakeshyadav-de@users.noreply.github.com>
2023-02-28 15:45:33 +0530

    Create 03. Find the employees who are inside hospital.sql

---
+++ 03. Find the employees who are inside hospital.sql
@@ -0,0 +1,17 @@
+--Given data has in and out entr of employees, write a query to find the employees who are inside the hospital. 
+
+create table hospital ( emp_id int
+, action varchar(10)
+, time datetime);
+
+insert into hospital values ('1', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('1', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('2', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:00:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:15:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('4', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('5', 'out', '2019-12-22 09:40:00');
